4f1b6e00290770b07fe90415a39fb81a334a7e43
WillianMassuki <willianmassuki@gmail.com>
2026-07-03 08:00:40 -0300

    configuração do Docker

--- src/main/java/br/com/ms_voo/MsVooApplication.java
+++ src/main/java/br/com/ms_voo/MsVooApplication.java
@@ -2,11 +2,13 @@ package br.com.ms_voo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
+@EnableFeignClients
 @SpringBootApplication
 public class MsVooApplication {
 
-	public static void main(String[] args) {
+	static void main(String[] args) {
 		SpringApplication.run(MsVooApplication.class, args);
 	}
 
